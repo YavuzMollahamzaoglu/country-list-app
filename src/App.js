dcfe5f04af10746dc278f7e0a72c376df847ce5e
Yavuz Mollahamzaoğlu <yavuz.mersin@hotmail.com>
2022-07-01 01:11:19 +0300

    Add country-name-app Text field and Button

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@mui/material";
 
 function App() {
-  const [name, setCountryName] = useState([]);
+  const [countryName, setCountryName] = useState("");
 
   const handleCountryNameChange = (e) => {
     setCountryName(e.target.value);
-    if(name && 0){
-      <TextField
-      name='name'
-      label='Name'
-      placeholder='Type your name'
-      value={this.state.fields.name}
-      onChange={this.onChange}
-      error={true} 
-    />
-
-    } 
   };
+
   return (
     <div>
       <TextField
-        
         id="standard-basic"
         label="Search Countries"
         variant="standard"
